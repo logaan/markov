9446e7c7a237435c24119136a3c049b100d11046
Logan Campbell <logaan@Logans-MacBook-Air.local>
2018-02-27 00:15:21 +1100

    from-string to lists

--- markov/src/markov/core.clj
+++ markov/src/markov/core.clj
@@ -1,6 +1,19 @@
-(ns markov.core)
+(ns markov.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def new-table
+  {})
+
+(defn inc-token [table [first second]]
+  (update-in table [first] conj second))
+
+(defn from-string [string]
+  (let [parts  (str/split string #" ")
+        tokens (concat [:start] parts [:end])
+        pairs  (partition 2 1 tokens)]
+    (reduce inc-token new-table pairs)))
+
+(defn from-string-golf [string]
+  (->> (concat [:start] (str/split string #" ") [:end])
+       (partition 2 1)
+       (reduce (fn [t [f s]] (update-in t [f] conj s)) {})))
--- markov/test/markov/core_test.clj
+++ markov/test/markov/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [markov.core :refer :all]))
 
+(def text0
+  "Hello world")
+
+(def parsed0
+  {:start  ["Hello"]
+   "Hello" ["world"]
+   "world" [:end]})
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Simplest case"
+    (is (= parsed0 (from-string text0)))
+    (is (= parsed0 (from-string-golf text0)))))
+
+(def text1
+  "The quick brown fox. Jumped over the lazy dog.")
+
+(def parsed1
+  {:start {"The" 1}})
+
+(def text2
+  "The slow brown fox. Jumped over many lazy dogs.")
