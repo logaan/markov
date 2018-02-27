93cd00604dab06c058f3d9de4e1a866109604ef2
Logan Campbell <colin@logaan.net>
2018-02-28 02:25:08 +1100

    hopefully a bit simpler

--- markov/src/markov/core.clj
+++ markov/src/markov/core.clj
@@ -1,30 +1,31 @@
 (ns markov.core
   (:require [clojure.string :as str]))
 
-(defn inc-token [table [first second]]
+(defn add-token [table [first second]]
   (update-in table [first] conj second))
+
+(defn from-readable [string]
+  (-> (str/lower-case string)
+      (str/replace #"\." " .")
+      (str/split #" ")))
+
+(defn to-readable [words]
+  (-> (str/join " " words)
+      (str/replace #" \." ".")))
 
 (defn from-string
   ([string]
    (from-string {} string))
 
   ([table string]
-   (let [parts (-> string
-                   (str/lower-case)
-                   (str/replace #"\." " .")
-                   (str/split #" "))
+   (let [parts  (from-readable string)
          tokens (concat [:start] parts [:end])
          pairs  (partition 2 1 tokens)]
-     (reduce inc-token table pairs))))
+     (reduce add-token table pairs))))
 
-(defn generate
-  ([table]
-   (let [joined (->> (generate table :start)
-                     (take-while #(not (= :end %)))
-                     (rest)
-                     (str/join " "))]
-     (str/replace joined #" \." ".")))
-
-  ([table word]
-   (let [next (rand-nth (table word))]
-     (cons word (lazy-seq (generate table next))))))
+(defn generate [table]
+  (->> :start
+       (iterate #(rand-nth (table %)))
+       (remove keyword?)
+       (take-while some?)
+       to-readable))
